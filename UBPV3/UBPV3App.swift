cdb2f9fa4b9d1388d484906570c01777a5af0dc5
Rale <rbjelic@icloud.com>
2022-09-02 12:28:28 +0200

    Initial Commit

---
+++ UBPV3/UBPV3App.swift
@@ -0,0 +1,17 @@
+//
+//  UBPV3App.swift
+//  UBPV3
+//
+//  Created by Ratko Bjelic on 2.9.22..
+//
+
+import SwiftUI
+
+@main
+struct UBPV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
